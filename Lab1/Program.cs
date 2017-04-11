25a7207c527da59258a105cb4be142ef10700722
Dennis <perk430@gmail.com>
2017-04-11 09:36:33 -0400

    initial commit

---
+++ Lab1/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Welcome to Grand Circus' Room Detail Generator!");
+
+            // Getting input from user
+
+            double length;
+
+            double width;
+
+            double area;
+
+            double perimeter;
+
+            string response = "Y";
+
+            do
+
+            {
+
+                Console.WriteLine("Please enter the length of the room: ");
+                length = double.Parse(Console.ReadLine());
+
+                Console.WriteLine("Please enter the width of the room: ");
+                width = double.Parse(Console.ReadLine());
+
+                // Calculating the area of the room
+
+                area = length * width;
+
+                Console.WriteLine("The area of the room is: " + area + " square units");
+
+                // Calulating the perimeter of the room
+
+                perimeter = 2 * length + 2 * width;
+
+                Console.WriteLine("The perimeter of the room is: " + perimeter + " units");
+
+                // Prompting to continue to measure rooms
+
+                Console.WriteLine("Would you like to continue? (Y/N)");
+                
+
+                response = Console.ReadLine();
+            } while (response.ToUpper() != "N");            
+        }
+    }
+}
